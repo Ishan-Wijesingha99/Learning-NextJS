e81443546b14ad9ae8852cf0d3d318ae62643b75
Ishan <ishanwijes@gmail.com>
2023-04-25 11:16:50 +1000

    added basic routing

--- learning-nextjs/pages/index.js
+++ learning-nextjs/pages/index.js
@@ -1,4 +1,6 @@
 import styles from '@/styles/Home.module.css'
+
+// this does the same thing as Link from react router dom
 import Link from 'next/link'
 
 // in normal react, we could add things to the head of the HTML by just navigating to the html file and adding it there, in NextJS we don't have that, so we do it this way
@@ -14,7 +16,7 @@ export default function Home() {
       </Head>
 
       <div className={styles.container}>
-        <h1>Yo</h1>
+        <h1 className={styles.HomePageTitle}>Yo</h1>
 
         <Link href="/about">About</Link>
       </div>
