21606052df22de959960ab14d20856e4b1378745
Ishan <ishanwijes@gmail.com>
2023-04-25 16:53:47 +1000

    completed server-side rendering function

--- learning-nextjs/pages/coins/index.js
+++ learning-nextjs/pages/coins/index.js
@@ -23,14 +23,33 @@ const CoinList = ({ coinData }) => {
 
 
 // static site generation (SSG)
-export const getStaticProps = async ({ params }) => {
+// can only use one of SSG or SSR, need to comment one out
+
+// export const getStaticProps = async () => {
+//   const ApiData = await Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
+
+//   return {
+//     props: {
+//       coinData: ApiData.data
+//     },
+//     // this allows you to refresh the page at the interval you define, in seconds
+//     revalidate: 10,
+//   }
+// }
+
+
+
+// server-side rendering (SSR)
+export const getServerSideProps = async () => {
   const ApiData = await Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
 
   return {
     props: {
-      coinData: ApiData.data
-    }
+      coinData: ApiData.data,
+    },
   }
 }
+
+
 
 export default CoinList
